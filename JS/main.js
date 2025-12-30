a84da00b163a41dfd0e6941b1b38a53f06e27358
tanatchaket-droid <tanatcha.ket@ku.th>
2025-12-30 11:05:24 +0700

    add js

---
+++ JS/main.js
@@ -0,0 +1,65 @@
+ /*console.log("Hello World");
+
+
+//String
+ let fname = "John";
+//Number
+ let age ="30";
+ let height = "5.9";
+
+ fname = "Alice";
+ console.log("Name:",fname);
+
+ fname="Bob";
+ PI = 3.14159;
+ console.log("Name:",fname);
+ console.log("Age:",age);
+ console.log("Height:",height);
+
+let Number1 = 10;
+let Number2 = 3;
+
+let result1 = Number1+Number2;
+console.log("ผลบวก =",result1);
+
+
+let number1 = 10;
+let number2 = 20;
+let condition = number1 < number2;
+console.log("condition:",condition);
+
+let number1 = 3;
+let number2 = 5;
+
+let score = 80;
+
+if(score >= 80){
+    console.log('A');
+}else if(score >= 70){
+    console.log('B');
+}else if(score >= 60){
+    console.log('c');
+}else if(score >= 50){
+    console.log('D');
+}else{
+    console.log('F');
+}
+
+let number1 = 5;
+let number2 = 10;
+let condition1 = (number1 > 0) || (number2 > 0)
+console.log("condition1:",condition1)
+
+let number1 = 5
+if(Number % 2 == 0){
+    console.log("Even")
+}
+*/
+let conter = 0;
+while(conter<=9){
+    conter+=1;
+    console.log("while:",conter);    
+}
+for(let i = 0; i <= 4; i = i + 1){
+    console.log("for:",i);
+}
