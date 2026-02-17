f4f9331ceef17c4e6fe2aaa94b2216c5e8c1c33f
tanatchaket-droid <tanatcha.ket@ku.th>
2026-02-17 11:38:17 +0700

    update api

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,61 @@
-const http = require('http');
-const host = 'localhost';
-const port = 8000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const app =express();
+const port = 8000
+app.use(bodyParser.json());
+let users =[]
+let counter = 1;
 
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end('My First Server!');
-}
-const Server = http.createServer(requestListener);
-Server.listen(port,host,()=>{
-    console.log(`Server is running at http://${host}:${port}`);
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+
+//path = POST
+app.post('/user', (req, res) => {
+    let user = req.body;
+    user.id = counter
+    counter += 1;
+    users.push(user);
+    res.json({
+        message: 'User added successfully',
+        user: user});
+})
+
+app.patch('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updatedUser = req.body;
+    let seletedIndex = users.findIndex(user => user.id == id);
+    if(updatedUser.name){
+        users[seletedIndex].name = updatedUser.name;
+    }
+    if(updatedUser.email){
+        users[seletedIndex].email = updatedUser.email;
+    }
+
+    )
+
+    users[seletedIndex].name = updatedUser.name || users[seletedIndex].name;
+    users[seletedIndex].email = updatedUser.email || users[seletedIndex].email;
+
+    res.json({
+         message: 'User updated successfully',
+        data:{
+            user: updatedUser,
+            indexUpdated: seletedIndex
+        }
+    });
+})
+app.delete('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let seletedIndex = users.findIndex(user => user.id == id);
+    delete users[seletedIndex];
+    users.splice(seletedIndex, 1);
+
+    res.json({  
+        message: 'User deleted successfully',
+        indexDeleted: seletedIndex
+    });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
