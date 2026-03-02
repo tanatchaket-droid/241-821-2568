b78a1bc849ea4933708bb96c604be5bb36825bf0
tanatchaket-droid <Tanatcha.ket@ku.th>
2026-03-02 18:24:42 +0700

    add

--- Backend/index.js
+++ Backend/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2/promise');
 const app = express();
+const cors = require('cors');
 const port = 8000
 
 app.use(bodyParser.json());
+app.use(cors());
 
+let users = []
+let counter = 1;
 let conn = null
+
 const initDBConnection = async () => {
     conn = await mysql.createConnection({
         host: 'localhost',
@@ -17,35 +22,44 @@ const initDBConnection = async () => {
     })
 }
 
-//path = GET / users เก็บข้อมูลทั้งหมดในรูปแบบ JSON
-app.get('/users/:id',async (req, res) => {
-    try {
+app.get('/test', (req, res) => {
+    res.send('Server is working!');
+});
+
+app.get('/users', async(req, res) => {
+    const results = await conn.query('SELECT * FROM users');
+    res.json(results[0]);
+})
+
+app.get('/users/:id', async (req, res) => {
+    try{
         let id = req.params.id
-        const results = await conn.query('SELECT * FROM users WHERE id = ?', [id]);
-        if (results[0].length === 0) {
-            throw{ statusCode: 404, message: 'User not found' };
+        const results = await conn.query('SELECT * FROM users WHERE id = ?', id)
+        if (results[0].length == 0) {
+            throw { statusCode : 404, message: 'User not found'};
         }
-         res.json(results[0][0]);
-    } catch (error) {
+        res.json(results[0][0]);
+    }
+    catch (error) {
         console.error('Error fetching user:', error.message);
-        let ststusCode = error.statusCode || 500;
-        res.status(ststusCode).json({
-            message:'Error fetching user',
-            error: error.message 
+        let statusCode = error.statusCode || 500;
+        res.status(statusCode).json({
+            message: 'Error fetching user',
+            error: error.message
         });
     }
 })
 
+// path = POST /users // การส่งข้อมูล บาง post สามารถดึงข้อมูลได้เหมือน get
 app.post('/users', async (req, res) => {
     try{
-     let user = req.body;
-     const results = await conn.query('INSERT INTO users SET ?', user);
-     console.log('results', results);
-     res.json({
-         message:'User created successfully',
-         data: results[0]    
-     });
-    }catch (error) {
+        let user = req.body;
+        const results = await conn.query('INSERT INTO users SET ?', user)
+        res.json({
+            message: 'User created successfuly',
+            data: results[0]   
+        })
+    } catch (error) {
         console.error('Error creating user:', error);
         res.status(500).json({
             message: 'Error creating user',
@@ -53,39 +67,44 @@ app.post('/users', async (req, res) => {
         });
     }
 });
+
+// path = PUT /user/:id // สำหรับการแก้ไขข้อมูล
 app.put('/users/:id', async (req, res) => {
-    try {
-        let id = req.params.id;
-        let updateUser = req.body;
-        const results = await conn.query('UPDATE users SET ? WHERE id = ?', [updateUser,id]);
+    try{
+        let id = req.params.id
+        let updatedUser = req.body;
+        const results = await conn.query('UPDATE users SET ? WHERE id = ?', [updatedUser, id])
         if (results[0].affectedRows == 0) {
-            throw { statusCode: 404, message: 'User not found' };
+            throw { statusCode : 404, message: 'User not found'};
         }
         res.json({
             message: 'User updated successfully',
-            data: updateUser
+            data: updatedUser
         });
-    } 
+    }
     catch (error) {
-        console.error('Error updating user:' ,error.message);
+        console.error('Error updating user:', error.message);
         let statusCode = error.statusCode || 500;
-        res.status(statusCode).json({ 
+        res.status(statusCode).json({
             message: 'Error updating user',
             error: error.message
         });
-     }
+    }
 })
+
+
+// path = DELETE /user/:id // สำหรับการลบข้อมูล
 app.delete('/users/:id', async (req, res) => {
-    try {
+    try{
         let id = req.params.id
         const results = await conn.query('DELETE FROM users WHERE id = ?', id)
         if (results[0].affectedRows == 0) {
-            throw { statusCode: 404, message: 'User not found' };
+            throw { statusCode : 404, message: 'User not found'};
         }
         res.json({
             message: 'User deleted successfully'
         });
-    } 
+    }
     catch (error) {
         console.error('Error deleting user:', error.message);
         let statusCode = error.statusCode || 500;
@@ -95,7 +114,8 @@ app.delete('/users/:id', async (req, res) => {
         });
     }
 })
+    
 app.listen(port, async () => {
-    await initDBConnection ();
+    await initDBConnection();
     console.log(`Server is running on port ${port}`)
 });
